0e546af22bdc1f04109dbf1f1e88427296b49ce5
Valentin Caure(tour) <valentin.caure@epitech.eu>
2022-06-21 00:19:26 +0200

    [FIX] some db issues

--- commands/diverse/getOpgg.py
+++ commands/diverse/getOpgg.py
@@ -10,12 +10,12 @@ async def getOpgg(self, ctx: Interaction, req: str, sep: str):
     msg: list[str] = ["Voici le(s) opgg demandé(s):"]
     multi = "https://euw.op.gg/multisearch/euw?summoners="
     for user_tag in req_list:
-        pos = checkIdExist(self.bot.db.users, user_tag)
+        pos = checkIdExist(self.bot.db.leaderboard.users, user_tag)
         if pos == -1:
             msg.append(f"{user_tag} semble ne pas avoir mit son opgg !")
         else:
-            msg.append(f"{user_tag} : {self.bot.db.users[pos].name}")
-            multi += f"{self.bot.db.users[pos].name},"
+            msg.append(f"{user_tag} : {self.bot.db.leaderboard.users[pos].name}")
+            multi += f"{self.bot.db.leaderboard.users[pos].name},"
     if multi != "https://euw.op.gg/multisearch/euw?summoners=":
         msg.append(f"multi opgg : {multi}")
     await ctx.response.send_message("\n".join(msg), ephemeral=True)
--- commands/leaderboard/setLeaderboardVisibility.py
+++ commands/leaderboard/setLeaderboardVisibility.py
@@ -2,9 +2,9 @@ from utils.leaderboard.checkIdExist import checkIdExist
 
 
 async def setLeaderboardVisibility(self, ctx, visible):
-    pos = checkIdExist(self.bot.db.users, ctx.user.mention)
+    pos = checkIdExist(self.bot.db.leaderboard.users, ctx.user.mention)
     if pos == -1:
         raise Exception("Tu n'es pas enregistré sur le leaderboard !")
-    self.bot.db.users[pos].is_displayed = visible
+    self.bot.db.leaderboard.users[pos].is_displayed = visible
     self.save()
     ctx.response.send_message("Ta visibilité sur le Leaderboard a été mis à jour !")
