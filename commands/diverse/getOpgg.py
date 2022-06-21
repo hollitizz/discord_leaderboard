e81f4d52a62bffa3a87fca90de2a75ea3feaf056
Valentin Caure <valentin.caure@epitech.eu>
2022-06-21 16:26:42 +0200

    [FIX] getOpgg

--- commands/diverse/getOpgg.py
+++ commands/diverse/getOpgg.py
@@ -1,20 +1,25 @@
-
-
-from discord import Interaction
-
+from discord import Interaction, Member
 from utils.leaderboard.checkIdExist import checkIdExist
-
+import re
 
 async def getOpgg(self, ctx: Interaction, req: str, sep: str):
-    req_list: list[str] = req.split(sep)
+    member_list: list[Member] = []
+    for string in req.split(sep):
+        try:
+            fetched_member: Member = await ctx.guild.fetch_member(re.sub("[^0-9]", "", string))
+        except:
+            continue
+        member_list.append(fetched_member)
     msg: list[str] = ["Voici le(s) opgg demandé(s):"]
     multi = "https://euw.op.gg/multisearch/euw?summoners="
-    for user_tag in req_list:
-        pos = checkIdExist(self.bot.db.leaderboard.users, user_tag)
+    if not member_list:
+        raise Exception("Aucune mention valide n'a été trouvé dans ton message")
+    for user_tag in member_list:
+        pos = checkIdExist(self.bot.db.leaderboard.users, user_tag.mention)
         if pos == -1:
-            msg.append(f"{user_tag} semble ne pas avoir mit son opgg !")
+            msg.append(f"{user_tag.mention} semble ne pas avoir mit son opgg !")
         else:
-            msg.append(f"{user_tag} : {self.bot.db.leaderboard.users[pos].name}")
+            msg.append(f"{user_tag.mention} : https://euw.op.gg/summoners/euw/{self.bot.db.leaderboard.users[pos].name}")
             multi += f"{self.bot.db.leaderboard.users[pos].name},"
     if multi != "https://euw.op.gg/multisearch/euw?summoners=":
         msg.append(f"multi opgg : {multi}")
