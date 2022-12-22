1346e8fc6a42cf9f84bf75794c9d01cde4f22984
Valentin Caure <valentin.caure@epitech.eu>
2022-12-22 23:30:42 +0100

    [FIX] stat refresh

--- janna/utils/leaderboard/refreshStats.py
+++ janna/utils/leaderboard/refreshStats.py
@@ -1,4 +1,4 @@
-from time import sleep
+from asyncio import sleep
 from utils.leaderboard.getPlayerStats import getPlayerStats
 from utils.myTypes import Setup
 
@@ -8,7 +8,7 @@ async def refreshStats(self: Setup):
 
     for i, user in enumerate(users):
         if ((i + 1) % 10 == 0):
-            sleep(1)
+            await sleep(1)
         try:
             user = await getPlayerStats(self.riot_token, user)
         except:
