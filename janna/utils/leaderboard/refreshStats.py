0ce534a80efc86bffb810a253d8aca9031148966
Valentin Caure <valentin.caure@epitech.eu>
2022-12-22 23:36:14 +0100

    [ADD] error message on get stats

--- janna/utils/leaderboard/refreshStats.py
+++ janna/utils/leaderboard/refreshStats.py
@@ -1,6 +1,10 @@
 from asyncio import sleep
 from utils.leaderboard.getPlayerStats import getPlayerStats
 from utils.myTypes import Setup
+from logging import getLogger
+
+
+_logger = getLogger(__name__)
 
 
 async def refreshStats(self: Setup):
@@ -11,5 +15,6 @@ async def refreshStats(self: Setup):
             await sleep(1)
         try:
             user = await getPlayerStats(self.riot_token, user)
-        except:
+        except Exception as e:
+            _logger.error(f"Error while refreshing stats for {user}: {e}")
             continue
