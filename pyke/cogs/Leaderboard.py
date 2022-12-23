1f77a99d6def7effe9613129661772cb580ea121
Valentin Caure <valentin.caure@epitech.eu>
2022-12-23 20:58:05 +0100

    [FIX]error handling

--- pyke/cogs/Leaderboard.py
+++ pyke/cogs/Leaderboard.py
@@ -1,4 +1,5 @@
 import sys
+import traceback
 from discord.ext import commands
 from discord import Member, app_commands, Interaction, Object
 from discord.app_commands import Choice
@@ -24,27 +25,32 @@ class Leaderboard(commands.Cog, description="Groupe de commandes du Leaderboard"
     async def registerError(self, ctx: Interaction, error: Exception):
         await ctx.response.send_message("an error occured !", ephemeral=True)
         _logger.error(f"{ctx.user} got : {error}")
+        traceback.print_exc()
 
     @app_commands.command(name="add_player", description="Ajoute un joueur au Leaderboard")
     @app_commands.default_permissions(manage_messages=True)
     @app_commands.checks.has_role("bot admin")
     async def addPlayer(self, ctx: Interaction, membre: Member, summoner_name: str):
+        _logger.info(f"{ctx.user} used  /{ctx.command.name} with ['{membre}', '{summoner_name}'] as arguments")
         await addPlayer.addPlayer(self.bot, ctx, membre, summoner_name)
 
     @addPlayer.error
     async def addPlayerError(self, ctx: Interaction, error: Exception):
         await ctx.response.send_message(f"an error occured", ephemeral=True)
         _logger.error(f"{ctx.user} got : {error}")
+        traceback.print_exc()
 
     @app_commands.command(name="set_leaderboard_visibility", description="Choisis si tu veux apparaître dans le Leaderboard")
     @app_commands.choices(visible=[Choice(name="Apparaître", value=1), Choice(name="Ne pas apparaître", value=0)])
     async def setLeaderboardVisibility(self, ctx: Interaction, visible: int):
+        _logger.info(f"{ctx.user} used  /{ctx.command.name} with ['{visible}'] as arguments")
         await setLeaderboardVisibility.setLeaderboardVisibility(self.bot, ctx, bool(visible))
 
     @setLeaderboardVisibility.error
     async def setLeaderboardVisibilityError(self, ctx: Interaction, error: Exception):
         await ctx.response.send_message(f"an error occured", ephemeral=True)
         _logger.error(f"{ctx.user} got : {error}")
+        traceback.print_exc()
 
 
 async def setup(bot: Setup):
