2fa0242e8f0ac528c6f9be67a22370f8886185e1
Hollitizz <valentin.caure@epitech.eu>
2023-03-18 00:23:54 +0100

    Update cleanSaveFolder.py

--- pyke/utils/cleanSaveFolder.py
+++ pyke/utils/cleanSaveFolder.py
@@ -8,7 +8,7 @@ import shutil
 _logger = getLogger(__name__)
 
 def cleanSaveFolder():
-    while len(os.listdir("./db_saves")) > 7:
+    while len(os.listdir("./db/db_saves")) > 7:
         oldest_file = min([datetime.strptime(f, "%d-%m-%Y.sql") for f in os.listdir("./db_saves") if f != "init.sql"])
         subprocess.Popen(
             f"rm -rf ./db_saves/{oldest_file.strftime('%d-%m-%Y')}.sql",
