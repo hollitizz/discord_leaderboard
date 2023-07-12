8cefe4f9eb928525a30886b318ce387b74915b02
Valentin Caure <valentin.caure@epitech.eu>
2023-07-12 20:58:54 +0200

    [FIX] response edit message to edit original response

--- pyke/commands/leaderboard/addPlayer.py
+++ pyke/commands/leaderboard/addPlayer.py
@@ -14,7 +14,7 @@ async def addPlayer(self: Setup, ctx: Interaction, member: Member, summoner_name
     await ctx.response.defer(ephemeral=True, thinking=True)
     league_id = await checkName(summoner_name)
     if not league_id:
-        await ctx.response.edit_message(content=f"{summoner_name}, ce compte doit être enregistré sur le serveur EUW .\n Vérifie l'orthographe et réessaye !")
+        await ctx.edit_original_response(content=f"{summoner_name}, ce compte doit être enregistré sur le serveur EUW .\n Vérifie l'orthographe et réessaye !")
         return
     if not self.db.checkUserExist(member.id):
         self.db.createUser(member.id)
@@ -23,8 +23,9 @@ async def addPlayer(self: Setup, ctx: Interaction, member: Member, summoner_name
         self.db.addAccountToUser(member.id, summoner_name, tier, rank, lp, league_id)
         await refreshUserRole(ctx.guild, member.id, tier)
     except AlreadyExists as e:
-        await ctx.response.edit_message(content=f"{e}")
+        await ctx.edit_original_response(content=f"{e}")
         return
     except Exception as e:
-        _logger.error(e)
-    await ctx.response.edit_message(content=f"{member.mention} est enregistré, il devrais apparaître dans le leaderboard d'ici 10 minutes !")
+        _logger.exception(e)
+        return
+    await ctx.edit_original_response(content=f"{member.mention} est enregistré, il devrais apparaître dans le leaderboard d'ici 10 minutes !")
